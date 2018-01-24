4ee063e3c957e06ca6680c658f42d08736e61322
sius <sius@gmx.net>
2018-01-24 23:04:25 +0100

    update readme

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ Things I miss in the Java World.
 ## Overview
 
 - infusion-lambda: An OO approach of the lambda calculus that includes currying
+
+### infusion-lambda
+
+**Example**
+
+```java
+// infusion-lambda/src/test7java/FuncTest.java
+
+Func4<String, Character, Boolean, Byte, String> f4 = (arg0, arg1, arg2, arg3) -> arg0 + arg1 + arg2 + arg3;
+
+assertEquals("abtrue127", f4.apply("a", 'b', true, Byte.MAX_VALUE));
+assertEquals("abtrue127", f4.apply("a", 'b', true).apply(Byte.MAX_VALUE));
+assertEquals("abtrue127", f4.apply("a", 'b').apply(true, Byte.MAX_VALUE));
+assertEquals("abtrue127", f4.apply("a", 'b').apply(true).apply(Byte.MAX_VALUE));
+assertEquals("abtrue127", f4.apply("a").apply('b',true, Byte.MAX_VALUE));
+assertEquals("abtrue127", f4.apply("a").apply('b', true).apply(Byte.MAX_VALUE));
+assertEquals("abtrue127", f4.apply("a").apply('b').apply(true).apply(Byte.MAX_VALUE));
+
+```
--- subprojects/infusion-lambda/src/test/java/infusion/lambda/FuncTest.java
+++ subprojects/infusion-lambda/src/test/java/infusion/lambda/FuncTest.java
@@ -29,11 +29,8 @@ public class FuncTest {
 	}
 	@Test
 	public void curryingF4() {
-		Func4<String, Character, Boolean, Byte, String> f4 = new Func4<String, Character, Boolean, Byte, String>() {
-			@Override public String apply(String arg0, Character arg1, Boolean arg2, Byte arg3) {
-				return arg0 + arg1 + arg2 + arg3;
-			}
-		};
+		Func4<String, Character, Boolean, Byte, String> f4 = (arg0, arg1, arg2, arg3) -> arg0 + arg1 + arg2 + arg3;
+
 		assertEquals("abtrue127", f4.apply("a", 'b', true, Byte.MAX_VALUE));
 		assertEquals("abtrue127", f4.apply("a", 'b', true).apply(Byte.MAX_VALUE));
 		assertEquals("abtrue127", f4.apply("a", 'b').apply(true, Byte.MAX_VALUE));
@@ -44,11 +41,7 @@ public class FuncTest {
 	}
 	@Test
 	public void curryingF5() {
-		Func5<String, Character, Boolean, Byte, Short, String> f5 = new Func5<String, Character, Boolean, Byte, Short, String>() {
-			@Override public String apply(String arg0, Character arg1, Boolean arg2, Byte arg3, Short arg4) {
-				return arg0 +  arg1 + arg2 + arg3 + arg4;
-			}
-		};
+		Func5<String, Character, Boolean, Byte, Short, String> f5 = (arg0, arg1, arg2, arg3, arg4) -> arg0 + arg1 + arg2 + arg3 + arg4;
 		assertEquals("abtrue12732767", f5.apply("a", 'b', true, Byte.MAX_VALUE, Short.MAX_VALUE));
 		assertEquals("abtrue12732767", f5.apply("a", 'b', true, Byte.MAX_VALUE).apply(Short.MAX_VALUE));
 		assertEquals("abtrue12732767", f5.apply("a", 'b', true).apply(Byte.MAX_VALUE, Short.MAX_VALUE));
@@ -70,38 +63,13 @@ public class FuncTest {
 		Func20<Character, Character, Character, Character, Character,
 				Character, Character, Character, Character, Character,
 				Character, Character, Character, Character, Character,
-				Character, Character, Character, Character, Character, String> f20 =
-					new Func20<Character, Character, Character, Character, Character,
-								Character, Character, Character, Character, Character,
-								Character, Character, Character, Character, Character,
-								Character, Character, Character, Character, Character, String>() {
+				Character, Character, Character, Character, Character, 
+				String> f20 = 
+					(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19) -> 
+						"" + c0 + c1 + c2 + c3+ c4 + c5 + c6 + c7 + c8+ c9 + c10 + c11 + c12 + c13+ c14 + c15 + c16 + c17 + c18 + c19;
+									
 
-									@Override
-									public String apply(
-											Character arg0,
-											Character arg1,
-											Character arg2,
-											Character arg3,
-											Character arg4,
-											Character arg5,
-											Character arg6,
-											Character arg7,
-											Character arg8,
-											Character arg9,
-											Character arg10,
-											Character arg11,
-											Character arg12,
-											Character arg13,
-											Character arg14,
-											Character arg15,
-											Character arg16,
-											Character arg17,
-											Character arg18,
-											Character arg19) {
-										return "" + arg0 + arg1 + arg2 + arg3+ arg4 + arg5 + arg6 + arg7 + arg8+ arg9 + arg10 + arg11 + arg12 + arg13+ arg14 + arg15 + arg16 + arg17 + arg18 + arg19;
-									}
 
-		};
 		String s = f20.apply('H').apply('e').apply('l').apply('l').apply('o').apply(' ').apply('W').apply('o').apply('r').apply('l').apply('d').apply('!').
 			apply(' ').apply(' ').apply('H').apply('e').apply('l').apply('l').apply('o').apply('!');
 		assertEquals("Hello World!  Hello!", s);
